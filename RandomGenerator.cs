9e543b92b02f2b676a4264e567f66053fcc3cd79
Owajeul Islam <owajeul01357@gmail.com>
2024-07-10 00:16:21 +0600

    add Random Generator

---
+++ RandomGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_Game
+{
+
+    internal class RandomGenerator
+    {
+        private Random RandomNumber { get; }
+        public RandomGenerator()
+        {
+            RandomNumber = new Random();
+        }
+
+        public int GetRandomNumber(int L, int R)
+        {
+            return RandomNumber.Next(L, R);
+        }
+
+        public byte[] GetRandomKey(int length)
+        {
+            byte[] key = new byte[length];
+            RandomNumberGenerator.Fill(key);
+            return key;
+        }
+
+
+        public string GetHMAC(string message, byte[] key)
+        {
+            using (var hmacsha3 = new HMACSHA3_256(key))
+            {
+                var hash = hmacsha3.ComputeHash(Encoding.UTF8.GetBytes(message));
+                return Convert.ToHexString(hash);
+            }
+        }
+
+    }
+}
